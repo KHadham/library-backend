bba7adc3533ed143aad98376f7c424128f71d0fa
kampretosjr <nightfury0896@gmail.com>
2019-08-09 17:37:39 +0700

    deploy

--- src/config/connect.js
+++ src/config/connect.js
@@ -1,17 +1,31 @@
 require('dotenv').config()
 const sql = require('mysql')
 
+// const connect = sql.createConnection({
+//     host        : process.env.DB_HOST,
+//     user        : process.env.DB_USER,
+//     password    : `${process.env.DB_PASS}`,
+//     database    : process.env.DB_NAME,
+// })
 const connect = sql.createConnection({
-    host        : process.env.DB_HOST,
-    user        : process.env.DB_USER,
-    password    : `${process.env.DB_PASS}`,
-    database    : process.env.DB_NAME,
+    host        : 'remotemysql.com',
+    user        : 'nqNstFQxSs',
+    password    : 'G0GwB8VvpH',
+    database    : 'nqNstFQxSs',
 })
-
 connect.connect((err) => {
     if (err) {
         throw err
     }
 })
+// Username: nqNstFQxSs
+
+// Database name: nqNstFQxSs
+
+// Password: G0GwB8VvpH
+
+// Server: remotemysql.com
+
+// Port: 3306
 
 module.exports = connect
